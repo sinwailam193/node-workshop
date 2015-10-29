1f30e942e888dbed4aa123059f0c4028f515ee7d
Sin Wai Lam <sinwailam193@hotmail.com>
2015-10-29 15:18:44 -0700

    solved first challenge

--- challenge1/start/app.js
+++ challenge1/start/app.js
@@ -5,25 +5,25 @@ var http = require('http'),
 http.createServer(function(request, response) {
 
     // TODO: Find the ASYNCHRONOUS, NON-BLOCKING API for reading in a file.
-    fs.CHANGE_ME('./data.csv', 'utf-8', function(err, data) {
+    fs.readFile('./data.csv', 'utf-8', function(err, data) {
         var responseData = {};
 
         // Basic JS: Work with the data in the file, and create the response
         var lines = data.split('\n');
+        console.log(lines);
 
         // Note the native forEach support in Arrays in node.js!
         lines.forEach(function(line) {
             var parts = line.split(',');
             responseData[parts[0]] = parts[1];
         });
-
         // TODO: How do we set the content type we're sending back?
         response.writeHead(200, {
-            'CHANGEME':'CHANGEME'
+            'content-type':'application/json'
         });
 
         // TODO: How do we serialize responseData to a JSON string?
-        response.end(CHANGE_ME.CHANGE_ME(responseData));
+        response.end(JSON.stringify(responseData));
 
     });
 
